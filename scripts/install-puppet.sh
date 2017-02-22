ab1f1f66adcc32be92ae8479a990504d1bc2badc
Ian Wright <ian.wright@well.ox.ac.uk>
2017-02-22 16:30:57 +0000

    Install and run puppet locally

---
+++ scripts/install-puppet.sh
@@ -0,0 +1,20 @@
+if [ $UID -ne 0 ]
+then
+	echo "Must be run as root"
+	exit 1
+fi
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+dpkg -i puppetlabs-release-pc1-xenial.deb
+gpg --keyserver pgp.mit.edu --recv-key 7F438280EF8D349F
+apt-get update
+apt-get install puppet-agent
+export PATH=$PATH:/opt/puppetlabs/bin
+#DO NOT RUN THIS
+#puppet resource package puppet ensure=latest
+cat ../provision/puppet/code/environments/production/hieradata/common.yaml >> /etc/puppetlabs/code/environments/production/hieradata/common.yaml
+cd ../provision/puppet/
+./install-modules.sh 
+cp -pr modules/* /etc/puppetlabs/code/modules/
+vi /etc/puppetlabs/code/environments/production/hieradata/common.yaml
+puppet apply -e "notice(lookup('sssd::config'))" 
+puppet apply  --verbose --detailed-exitcodes sso-ldap-client.pp 
