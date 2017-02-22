198062d5bd4f95c15a0184f3aac8f4f0a74d9984
Ian Wright <ian.wright@well.ox.ac.uk>
2017-02-22 16:38:03 +0000

    Change so it doesn't matter which directory it is run from

--- scripts/install-puppet.sh
+++ scripts/install-puppet.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 if [ $UID -ne 0 ]
 then
 	echo "Must be run as root"
 	exit 1
 fi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 dpkg -i puppetlabs-release-pc1-xenial.deb
 gpg --keyserver pgp.mit.edu --recv-key 7F438280EF8D349F
@@ -11,10 +13,10 @@ apt-get install puppet-agent
 export PATH=$PATH:/opt/puppetlabs/bin
 #DO NOT RUN THIS
 #puppet resource package puppet ensure=latest
-cat ../provision/puppet/code/environments/production/hieradata/common.yaml >> /etc/puppetlabs/code/environments/production/hieradata/common.yaml
-cd ../provision/puppet/
+cd ${DIR}/../provision/puppet/
+cat code/environments/production/hieradata/common.yaml >> /etc/puppetlabs/code/environments/production/hieradata/common.yaml
 ./install-modules.sh 
 cp -pr modules/* /etc/puppetlabs/code/modules/
 vi /etc/puppetlabs/code/environments/production/hieradata/common.yaml
-puppet apply -e "notice(lookup('sssd::config'))" 
+puppet apply -e "notice(lookup('sssd::config::domain::malariagen::ldap_uri'))" 
 puppet apply  --verbose --detailed-exitcodes sso-ldap-client.pp 
