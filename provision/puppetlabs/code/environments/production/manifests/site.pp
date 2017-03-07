82fe72dec340ee793f5767e22a7261067e0c34cd
Ian Wright <ian.wright@well.ox.ac.uk>
2017-03-07 14:00:39 +0000

    Move to hiera version 5

--- provision/puppetlabs/code/environments/production/manifests/site.pp
+++ provision/puppetlabs/code/environments/production/manifests/site.pp
@@ -1,35 +1,28 @@
- class { '::etckeeper':
-    vcs => 'git',
- }
 
+class { '::etckeeper':
+  vcs => 'git',
+}
 
 include sudo
 include sso_ldap_client
+
 include mail
+
 
 node default {
 }
 
 node sierra.well.ox.ac.uk {
+        postfix::config {
+                'header_checks': value => 'regexp:/etc/postfix/header_checks'
+        }
+        $header_checks = lookup({ 'name' => 'config::header_checks'})
+        file {
+                '/etc/postfix/header_checks':
+                content => "${header_checks}"
+        }
+        
 
-	postfix::config { 
-		'header_checks': value => 'regexp:/etc/postfix/header_checks'
-	}
-	$header_checks = lookup({ 'name' => 'config::header_checks'})
-	file {
-		'/etc/postfix/header_checks':
-		content => "${header_checks}"
-	}
+        include elk_server
 
-	include elk_server
-
-	class { 'hiera':
-		hierarchy => [
-			'secure',
-			'nodes/%{::trusted.certname}',
-			'%{environment}',
-			'common'
-		],
-		eyaml => true,
-	}
 }
