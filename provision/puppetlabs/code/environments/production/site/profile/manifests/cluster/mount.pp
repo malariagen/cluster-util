c1ec5d17a7296baed2074314146e55438ebeb81d
Ian Wright <ian.wright@well.ox.ac.uk>
2017-03-15 13:50:07 +0000

    First pass at mount points

--- provision/puppetlabs/code/environments/production/site/profile/manifests/cluster/mount.pp
+++ provision/puppetlabs/code/environments/production/site/profile/manifests/cluster/mount.pp
@@ -1,9 +1,40 @@
 class profile::cluster::mount(
+    $users,
+    $level1,
+    $level2,
+    Optional[String] $home_server = 'papa.well.ox.ac.uk',
+    Optional[String] $level1_server = 'papa.well.ox.ac.uk',
+    Optional[String] $level2_server = 'papa.well.ox.ac.uk',
 ) {
 
-    $user = 'iwright'
-    profile::cluster::home { 'home':
-        user => "${user}"
+    $nfs_options = "rw,rsize=32768,wsize=32768,intr,noatime,nfsvers=3,soft,bg,actimeo=0,timeo=600"
+
+    $users.each |String $user| {
+        mount { "/home/${user}":
+                    device  => "${home_server}:/homes/${user}",
+                    fstype  => "nfs",
+                    ensure  => "mounted",
+                    options => $nfs_options,
+                    atboot  => false,
+        }
     }
 
+    $level1.each |String $dir| {
+        mount { "/kwiat/1/${dir}":
+                    device  => "${level1_server}:/kwiat/1/${dir}",
+                    fstype  => "nfs",
+                    ensure  => "mounted",
+                    options => $nfs_options,
+                    atboot  => false,
+        }
+    }
+    $level2.each |String $dir| {
+        mount { "/kwiat/2/${dir}":
+                    device  => "${level2_server}:/kwiat/2/${dir}",
+                    fstype  => "nfs",
+                    ensure  => "mounted",
+                    options => $nfs_options,
+                    atboot  => false,
+        }
+    }
 }
