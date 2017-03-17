0efdba0915b2e8ea58436ac103df74b32fffeb66
Ian Wright <ian.wright@well.ox.ac.uk>
2017-03-17 15:20:01 +0000

    Change nfs approach

--- provision/puppetlabs/code/environments/production/site/profile/manifests/cluster/mount.pp
+++ provision/puppetlabs/code/environments/production/site/profile/manifests/cluster/mount.pp
@@ -5,36 +5,46 @@ class profile::cluster::mount(
     Optional[String] $home_server = 'papa.well.ox.ac.uk',
     Optional[String] $level1_server = 'papa.well.ox.ac.uk',
     Optional[String] $level2_server = 'papa.well.ox.ac.uk',
+    Optional[String] $cluster_server = 'quebec.well.ox.ac.uk',
 ) {
+
 
     $nfs_options = "rw,rsize=32768,wsize=32768,intr,noatime,nfsvers=3,soft,bg,actimeo=0,timeo=600"
 
+
+    class { '::nfs':
+          client_enabled => true,
+    }
+
+    nfs::client::mount{ '/var/lib/gridengine/default':
+        server => $cluster_server,
+        ensure  => 'mounted',
+        options_nfs => $nfs_options
+    }
+
+
     $users.each |String $user| {
-        mount { "/home/${user}":
-                    device  => "${home_server}:/homes/${user}",
-                    fstype  => "nfs",
-                    ensure  => "mounted",
-                    options => $nfs_options,
-                    atboot  => false,
+        nfs::client::mount{ "/home/${user}":
+            server => $home_server,
+            share => "/homes/${user}",
+            ensure  => 'mounted',
+            options_nfs => $nfs_options
         }
     }
 
     $level1.each |String $dir| {
-        mount { "/kwiat/1/${dir}":
-                    device  => "${level1_server}:/kwiat/1/${dir}",
-                    fstype  => "nfs",
-                    ensure  => "mounted",
-                    options => $nfs_options,
-                    atboot  => false,
+        nfs::client::mount{ "/kwiat/1/${dir}":
+            server => $level1_server,
+            ensure  => 'mounted',
+            options_nfs => $nfs_options
         }
     }
+
     $level2.each |String $dir| {
-        mount { "/kwiat/2/${dir}":
-                    device  => "${level2_server}:/kwiat/2/${dir}",
-                    fstype  => "nfs",
-                    ensure  => "mounted",
-                    options => $nfs_options,
-                    atboot  => false,
+        nfs::client::mount{ "/kwiat/2/${dir}":
+            server => $level2_server,
+            ensure  => 'mounted',
+            options_nfs => $nfs_options
         }
     }
 }
