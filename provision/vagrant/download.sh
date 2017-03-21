5ad9c2b3e8d2c7d3eceeca3c7a933dea9fc64544
Ian Wright <ian.wright@well.ox.ac.uk>
2017-03-21 14:38:49 +0000

    Remove scripts for manual install of sge

--- provision/vagrant/download.sh
+++
@@ -1,6 +0,0 @@
-wget http://arc.liv.ac.uk/downloads/SGE/releases/8.1.9/debian.tar.gz
-
-for i in sge-common_8.1.9_all.deb sge-doc_8.1.9_all.deb sge_8.1.9_amd64.deb sge-dbg_8.1.9_amd64.deb
-do
-    wget http://arc.liv.ac.uk/downloads/SGE/releases/8.1.9/$i
-done
