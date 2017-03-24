4f887719beb8f34b8a9eb4acb6d3d8d5a5fbd460
Ian Wright <ian.wright@well.ox.ac.uk>
2017-03-24 12:17:07 +0000

    All in puppet now

--- scripts/dependencies.sh
+++
@@ -1,6 +0,0 @@
-DEBIAN_FRONTEND=noninteractive apt-get install -y etckeeper
-add-apt-repository -y ppa:webupd8team/java
-DEBIAN_FRONTEND=noninteractive apt-get update
-echo debconf shared/accepted-oracle-license-v1-1 select true |  sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-DEBIAN_FRONTEND=noninteractive apt-get install -y oracle-java8-installer
--- scripts/run-local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source ${DIR}/local-settings.sh
-${DIR}/$1
-
